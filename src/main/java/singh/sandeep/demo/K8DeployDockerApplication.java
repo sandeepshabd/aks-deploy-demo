a4d380c7000f74291c5370433fc336a94ef905b1
Sandeep Singh <sandeepshabd@gmail.com>
2024-01-29 23:45:45 -0600

    initial commit

---
+++ src/main/java/singh/sandeep/demo/K8DeployDockerApplication.java
@@ -0,0 +1,13 @@
+package singh.sandeep.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8DeployDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8DeployDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/singh/sandeep/demo/K8DeployDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package singh.sandeep.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8DeployDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
